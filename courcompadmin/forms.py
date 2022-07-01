26bb474412499bf3c170190f4767bd98b316c3d9
RajagopalAswin1994 <rajagopal3806@gmail.com>
2022-07-01 16:02:49 +0530

    widgets updated form update on-process

--- courcompadmin/forms.py
+++ courcompadmin/forms.py
@@ -1,4 +1,6 @@
+from attr import attr, attrs
 from django import forms
+from matplotlib import widgets
 from courcompadmin.models import *
 
 class Course_Details_Form(forms.ModelForm):
@@ -6,3 +8,17 @@ class Course_Details_Form(forms.ModelForm):
     class Meta:
         model = Course_Details
         fields = "__all__"
+        widgets = {
+            'course_id': forms.TextInput(attrs={'class':'form-control'}),
+            'institute_id': forms.ChoiceField(attrs={'class':'form-control'}),
+            'course_name': forms.TextInput(attrs={'class':'form-control'}),
+            'What_you_learn': forms.TextInput(attrs={'class':'form-control'}),
+            'description': forms.TextInput(attrs={'class':'form-control'}),
+            'price': forms.IntegerField(attrs={'class':'form-control'}),
+            'duration': forms.TextInput(attrs={'class':'form-control'}),
+            'duration_type': forms.TextInput(attrs={'class':'form-control'}),
+            'course_through': forms.TextInput(attrs={'class':'form-control'}),
+            'course_type': forms.TextInput(attrs={'class':'form-control'}),
+            'course_category': forms.TextInput(attrs={'class':'form-control'})
+            # 'course_banner': forms.ImageField(attrs={'class':'form-control'})
+        }        
