93180c85c9aab72ffbca9bf7d8c587f7dfaa98df
Rajagopal <reactlerdeveloper@gmail.oom>
2022-07-06 20:02:31 +0530

    updated dummy files

--- test.py
+++
@@ -1,4 +0,0 @@
-BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR_TEM = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_DIR = os.path.join(BASE_DIR_TEM,'templates')
-STATIC_DIR = os.path.join(BASE_DIR_TEM,'static')
