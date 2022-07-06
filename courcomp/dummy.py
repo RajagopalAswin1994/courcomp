ef2401ccedca56ed09a8938cd0bbf04f611ee6cd
Rajagopal <reactlerdeveloper@gmail.oom>
2022-07-06 20:01:50 +0530

    updated dummy files

---
+++ courcomp/dummy.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+import os
+from posixpath import join
+
+
+print(os.path.dirname(os.path.abspath(__file__)))
---
+++ test.py
@@ -0,0 +1,4 @@
+BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR_TEM = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR_TEM,'templates')
+STATIC_DIR = os.path.join(BASE_DIR_TEM,'static')
